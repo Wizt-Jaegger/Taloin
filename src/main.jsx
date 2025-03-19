969d0fabc248c0f390561019b12a0aaa87496cad
Wizt <emireyesgutierres@gmail.com>
2025-03-19 12:23:56 -0600

    privacy terms

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { LanguageProvider } from "./LanguageContext.jsx"; 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { LanguageProvider } from "./LanguageContext"; 
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </LanguageProvider>
-  </StrictMode>,
-)
- 
+    
+  </StrictMode>
+);
